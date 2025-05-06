10d0b473289d19224525288087771a031f1b5916
alifhaider <alifhaider57@gmail.com>
2025-05-06 23:52:27 +0600

    add brand color

--- app/utils/extended-theme.ts
+++ app/utils/extended-theme.ts
@@ -11,6 +11,10 @@ export const extendedTheme = {
 			DEFAULT: 'hsl(var(--ring))',
 			invalid: 'hsl(var(--foreground-destructive))',
 		},
+		brand: {
+			DEFAULT: 'hsl(var(--brand))',
+			foreground: 'hsl(var(--brand-foreground))',
+		},
 		background: 'hsl(var(--background))',
 		foreground: {
 			DEFAULT: 'hsl(var(--foreground))',
