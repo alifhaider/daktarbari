23ca815a66968fbe13a05fdcaefdffb8b7454ee7
alifhaider <alifhaider57@gmail.com>
2025-05-09 05:15:33 +0600

    delete note related things

--- app/routes/resources+/download-user-data.tsx
+++ app/routes/resources+/download-user-data.tsx
@@ -21,18 +21,18 @@ export async function loader({ request }: Route.LoaderArgs) {
 					objectKey: true,
 				},
 			},
-			notes: {
-				include: {
-					images: {
-						select: {
-							id: true,
-							createdAt: true,
-							updatedAt: true,
-							objectKey: true,
-						},
-					},
-				},
-			},
+			// notes: {
+			// 	include: {
+			// 		images: {
+			// 			select: {
+			// 				id: true,
+			// 				createdAt: true,
+			// 				updatedAt: true,
+			// 				objectKey: true,
+			// 			},
+			// 		},
+			// 	},
+			// },
 			password: false, // <-- intentionally omit password
 			sessions: true,
 			roles: true,
@@ -50,13 +50,13 @@ export async function loader({ request }: Route.LoaderArgs) {
 						url: domain + getUserImgSrc(user.image.objectKey),
 					}
 				: null,
-			notes: user.notes.map((note) => ({
-				...note,
-				images: note.images.map((image) => ({
-					...image,
-					url: domain + getNoteImgSrc(image.objectKey),
-				})),
-			})),
+			// notes: user.notes.map((note) => ({
+			// 	...note,
+			// 	images: note.images.map((image) => ({
+			// 		...image,
+			// 		url: domain + getNoteImgSrc(image.objectKey),
+			// 	})),
+			// })),
 		},
 	})
 }
